e4cae9c6a6e391f092b27a00d506369d49da9b6e
Jason0705 <6jasonli6@gmail.com>
2018-06-19 16:03:24 -0700

    Convert to MVC design pattern.

---
+++ Listem/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Listem
+//
+//  Created by Jason Li on 2018-06-19.
+//  Copyright © 2018 Jason Li. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
